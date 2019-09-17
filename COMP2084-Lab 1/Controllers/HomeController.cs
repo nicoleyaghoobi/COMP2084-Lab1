ff7f4e3bf1eed825c8df842194bec7c0f83be40a
nicoleyaghoobi <53989251+nicoleyaghoobi@users.noreply.github.com>
2019-09-16 20:46:02 -0400

    initial commit

---
+++ COMP2084-Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMP2084_Lab_1.Models;
+
+namespace COMP2084_Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Addition(int a,int b)
+        {
+           
+            return Ok("result = "+a.ToString()+" + "+b.ToString()+" = " + (a+b).ToString()) ;
+        }
+        public IActionResult Multipication(int a, int b)
+        {
+
+            return Ok("result = " + a.ToString() + " * " + b.ToString() + " = " + (a * b).ToString());
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
